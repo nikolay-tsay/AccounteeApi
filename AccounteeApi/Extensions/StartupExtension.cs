0ea01edda85f7110505840b0506b1f14a326f38e
nikolay-tsay <tsay.nikolay99@gmail.com>
2022-12-23 18:39:56 +0100

    Project structure

--- AccounteeApi/Extensions/StartupExtension.cs
+++ AccounteeApi/Extensions/StartupExtension.cs
@@ -1,6 +1,24 @@
 ﻿namespace AccounteeApi.Extensions;
 
-public class StartupExtension
+public static class StartupExtension
 {
-    
+    public static void AddServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+    }
+
+    public static void SetupPipeline(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+    }
 }
